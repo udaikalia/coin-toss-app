bb50f2ef7a044502b0385aaba0f0ab59dfb0125a
udaikalia <udaikalia2002@gmail.com>
2022-10-15 13:44:37 +0530

    Initial Commit

---
+++ coin toss appTests/coin_toss_appTests.swift
@@ -0,0 +1,33 @@
+//
+//  coin_toss_appTests.swift
+//  coin toss appTests
+//
+//  Created by Udai Kalia on 15/10/22.
+//
+
+import XCTest
+@testable import coin_toss_app
+
+class coin_toss_appTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
